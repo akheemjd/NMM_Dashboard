ab2d6b0cc5c8cd85f55658174d6b9444b5f7b042
Akheem <akheemjd@gmail.com>
2026-08-01 21:29:28 -0400

    Restore NMDI with full form Northern Mile Diesel Index in brackets

--- scripts/build_templates.py
+++ scripts/build_templates.py
@@ -152,6 +152,7 @@ def build_all():
         "market-pulse": load_json("market.norm"),
         "industry-news": load_json("news.norm"),
         "fuel-cost-calculator": {**load_json("fx.norm"), **load_json("fuel.norm")},
+        "methodology": load_json("home.norm"),
     }
 
     built = []
